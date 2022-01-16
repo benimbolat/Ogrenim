c93be4d78a715c90df775bff8b1b0e242a680f08
Enes Bolat <benimbolat@gmail.com>
2022-01-16 23:17:27 +0300

    .

--- Flutter/UdemyFlutterDersleri/odev7/odev7.dart
+++ Flutter/UdemyFlutterDersleri/odev7/odev7.dart
@@ -3,6 +3,6 @@ import 'odev7ek.dart';
 void main(List<String> args) {
   // kota değeri girecek
   var geriDonus = odev7();
-  int ucret = geriDonus.kotaUcretHesaplama(62);
+  int ucret = geriDonus.kotaUcretHesaplama(51);
   print("Ödemeniz gerekn ücret = $ucret TL");
 }
